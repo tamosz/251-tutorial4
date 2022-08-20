f8de1913e1e3a55869f4bd80d23b5de246a04e86
Tomasz McDowell <mcdowelltomasz@gmail.com>
2022-08-20 19:52:27 +1200

    Initial commit

---
+++ src/main/java/tutorials/Calc.java
@@ -0,0 +1,10 @@
+package tutorials;
+public class Calc {
+    public static int add(int x, int y){
+        return x + y;
+    }
+    
+    public int subtract (int x, int y){
+        return x + y;
+    }
+}
---
+++ src/test/java/tutorials/AppTest.java
@@ -0,0 +1,14 @@
+package tutorials;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void addition() {
+        assertEquals(12, Calc.add(2,10));
+    }
+
+}
+
